4c07b23cd8973d1a88fd512be40b361063aaceb8
kartik <kartikbhutra02@gmail.com>
2025-08-23 22:07:18 +0530

    added electron

---
+++ electron/build.ts
@@ -0,0 +1,18 @@
+import { app, BrowserWindow } from "electron";
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  win.loadFile("../dist/index.html");
+}
+
+app.whenReady().then(() => {
+  createWindow();
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
